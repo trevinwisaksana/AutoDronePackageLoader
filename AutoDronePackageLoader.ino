fc8a07ab8041c955438246270a9ab972b224f14f
Trevin Wisaksana <trevinwisaksana@gmail.com>
2017-02-27 23:26:32 -0800

    Created a state machine

--- AutoDronePackageLoader.ino
+++ AutoDronePackageLoader.ino
@@ -1,27 +1,10 @@
 /*
 
-Code for servo calibration
-by eLab Peers (C) 2014.
 
-Visit us at:
-http://www.elabpeers.com
-
-All rights reserved.
-
-Wiring:
-For the servo
-1. Orange wire on servo to Pin 10
-2. Brown wire to GND on Arduino board or both GND on Arduino board and power source
-3. Red wire to 5V on Arduino board or + of the power source
-
-Features:
-1. The servo moves to 90 degree.
-2. 147 == 180 Degrees
-3. 65 == 90 Degrees
-4.
 */
 
 #include <Servo.h>
+#include <math.h>
 
 Servo baseServo;
 Servo shoulderServo;
@@ -30,29 +13,88 @@ Servo armServo;
 Servo wristServo;
 Servo fingerServo;
 
-void setup()
-{
+int baseCount;
+int shoulderCount;
+int elbowCount;
+int armCount;
+int wristCount;
+int fingerCount;
+
+char fingerState;
+char shoulderState;
+
+void setup() {
+
+  // Setting the baud rate
   Serial.begin(9600);
+
+  // Setting pin numbers of servos
   baseServo.attach(10);
   shoulderServo.attach(9);
   elbowServo.attach(8);
   armServo.attach(7);
   wristServo.attach(6);
   fingerServo.attach(5);
+ 
+  // Setup servo states 
+  // fingerState = 'U';
+  shoulderState = 'H';
+ 
 }
 
-void loop()
-{
+void loop() {
+  
+  /* Testing purposes
   // BASE SERVO can only go between 10 - 170
-  baseServo.write(90); 
   // SHOULDER SERVO can only go between 90 - 175
-  shoulderServo.write(90);
   // ELBOW SERVO can only go between 10 - 90
-  elbowServo.write(90);
   // ARM SERVO can only go between 60 - 180
-  armServo.write(180);
-  // WRIST SERVO can only go between 
-  wristServo.write(90);
-  // FINGER SERVO can only go between 0 - 50
-  fingerServo.write(0);
+  // WRIST SERVO can only go between 0 - 180
+  // FINGER SERVO can only go between 0 - 40
+  */
+
+  switch (shoulderState) {
+    case 'S': // STRAIGHT
+      shoulderCount = shoulderServo.read();
+      if ((shoulderCount > 0) && (shoulderCount < 175)) {
+        if (shoulderCount > 90) {
+          shoulderCount -= 1;
+        } else if (shoulderCount == 90) {
+          shoulderCount += 0;
+        } else {
+          shoulderCount += 1;
+        }
+        shoulderServo.write(shoulderCount);
+      }
+      break;
+    case 'H': // HIGHEST POINT
+      shoulderCount = shoulderServo.read();
+      // SHOULDER COUNT CANNOT BE 175 IF ARM SERVO IS NOT 180
+      if ((shoulderCount > 0) && (shoulderCount < 91)) {
+        shoulderCount += 1;
+        shoulderServo.write(shoulderCount);
+      }
+      break;
+  }
+
+  switch (fingerState) {
+    case 'G': // GRIP
+      // Assigning the fingerCount to its lowest value
+      fingerCount = 1;
+      // Incrementing the finger count by 1 
+      if ((fingerCount > 0) && (fingerCount < 40)) {
+        fingerCount += 1;
+        fingerServo.write(fingerCount);
+      }
+      break;
+    case 'U': // UNGRIP
+      // Assigning the fingerCount to its highest value - 1
+      fingerCount = 39;
+      if ((fingerCount > 0) && (fingerCount < 40)) {
+        fingerCount -= 1;
+        fingerServo.write(fingerCount);
+      }
+      break;
+  }
+  
 }
